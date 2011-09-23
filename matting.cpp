5cfab30d41ccd7916cc6bcd03682ba922a7e112a
Jan Rüegg <rggjan@gmail.com>
2011-09-23 14:36:51 +0200

    + Can do some calculations

--- matting.cpp
+++ matting.cpp
@@ -47,17 +47,40 @@ int main() {
   unsigned char* mask = load_image("trimap.pnm", width, height, 1);
   unsigned char* final = new unsigned char[width*height*3];
 
+  int fg[3];
+  fg[0] = 0;
+  fg[1] = 0;
+  fg[2] = 0;
+  int fg_num = 0;
+
+  int bg[3];
+  bg[0] = 0;
+  bg[1] = 0;
+  bg[2] = 0;
+  int bg_num = 0;
+
   for (int y=0; y<height; y++) {
     for (int x=0; x<width; x++) {
-      for (int color=0; color<3; color++) {
-        int index = y*width*3+x*3+color;
-        if (mask[y*width+x] == 0)
-          final[index] = original[index];
-        else
-          final[index] = 0;
+      int index = y*width+x;
+      if (mask[index] == 0) {
+        bg_num++;
+        for (int color=0; color<3; color++) {
+          bg[color] += original[index*color+color];
+        }
+      } else if (mask[index] == 255) {
+        fg_num++;
+        for (int color=0; color<3; color++) {
+          fg[color] += original[index*color+color];
+        }
       }
     }
   }
+
+  printf("fg: %i/%i/%i\n", fg[0]/fg_num, fg[1]/fg_num, fg[2]/fg_num);
+  printf("%%fg: %f\n", ((double)fg_num)/((double)width*height)*100);
+
+  printf("bg: %i/%i/%i\n", bg[0]/bg_num, bg[1]/bg_num, bg[2]/bg_num);
+  printf("%%bg: %f\n", ((double)bg_num)/((double)width*height)*100);
 
   save_image("final.ppm", width, height, 3, final);
 
